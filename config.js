0ced4d0c785edce35df262930d2231a462066f0a
Harsh Mohan <harshmohan813@gmail.com>
2020-06-12 00:11:31 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,105 @@
+var config = {};
+/* everything should be inside the double quotes and dont edit anything except things inside the double quotes */
+
+/* account name is your username which you uses to login steam */
+
+config.accountName = "YOUR_ACCOUNT_NAME_HERE";
+
+
+/* same password should be used, by which you login your steam account */
+
+config.password = "YOUR_PASSWORD_HERE";
+
+
+/* messages you want to be displayed when someone messages you when you are idle */
+/* use '\t' for a space in the message and '\n' for a new line */
+config.message = "YOUR_MESSAGE_HERE";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = config;
